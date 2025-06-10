c4733f604f5fbfe27562378836760d9d4e2bac74
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2025-06-10 15:35:02 +0200

    feat: refactor Postgres configuration and update app to use new environment variables

--- src/app.js
+++ src/app.js
@@ -2,9 +2,15 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.EXPRESS_PORT || 3000
+const { pool } = require('./postgres.config')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/', async (req, res) => {
+    await pool.connect()
+
+    const result = await pool.query('SELECT * FROM documents')
+    console.log(result)
+
+    // await pool.end()
 })
 
 app.listen(port, () => {
--- src/postgres.config.js
+++ src/postgres.config.js
@@ -1,2 +1,8 @@
 require('dotenv').config()
+const pg = require('pg')
+const { Pool, Client } = pg
 
+const pool = new Pool()
+const client = new Client()
+
+module.exports = { pool, client }
